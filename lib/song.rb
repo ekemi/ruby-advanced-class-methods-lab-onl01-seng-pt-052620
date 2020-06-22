0b3b8ccdceea001da9940efa5046b6f963938b6d
edmundo papi <edmundopierre@yahoo.com>
2020-06-22 11:25:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,51 @@ class Song
   def save
     self.class.all << self
   end
+def self.create
+song = Song.new
+@@all << song
+song
+end
+def self.new_by_name(song_name)
+song = Song.new
+song.name= song_name
+song
+end
 
+def self.create_by_name(nam)
+song = Song.create
+song.name = nam
+song
+end
+
+def self.find_by_name(song_name)
+
+@@all.find{ |value| value.name == song_name}
+
+end
+def self.find_or_create_by_name(song_name)
+  self.find_by_name(song_name)||self.create_by_name(song_name)
+
+  end
+  def self.alphabetical
+    @@all.sort_by {|value| value.name}
+
+  end
+
+  def self.new_from_filename(file_name)
+       song = self.new
+       x = (file_name.split("-").join).split("  ")
+
+       x.each do|value|
+         if value == x[0]
+           song.artist_name = value
+
+        else
+          y = value.split(".mp3").join
+          song.name = y
+        end
+
+  end
+  song
+end
 end
